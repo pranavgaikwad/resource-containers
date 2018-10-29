717685b7aff85133fbef06490968859f3dbe90f6
Pranav Gaikwad <gaikwad.pranav@gmail.com>
2018-10-28 22:14:32 -0400

    added line after debug output, added object offset

--- benchmark/benchmark.c
+++ benchmark/benchmark.c
@@ -75,7 +75,7 @@ void _test_mem_container(
     // generate a random number to write into the object.
     a = rand() + 1;
 
-    fprintf(stderr, "[Before] Container : %d, Data : %s\n", cid, mapped_data);
+    fprintf(stderr, "[Before] Container : %d, Offset : %d, Data : %s\n", cid, object_offset, mapped_data);
 
     // starts to write the data to that address.
     gettimeofday(current_time, NULL);
@@ -87,8 +87,10 @@ void _test_mem_container(
     strncpy(mapped_data, data, max_size_of_objects-1);
     mapped_data[max_size_of_objects-1] = '\0';
 
-    fprintf(stderr, "[After] Container : %d, Data : %s\n", cid, mapped_data);
+    fprintf(stderr, "[After] Container : %d, Offset : %d, Data : %s\n", cid, object_offset, mapped_data);
         
+    fprintf(stderr, "___________________________________________________________________________________________\n");
+
     fprintf(log_file, "S\t%d\t%d\t%ld\t%d\t%d\t%s\n", getpid(), cid, current_time->tv_sec * 1000000 + current_time->tv_usec, object_offset, max_size_of_objects, mapped_data);
     mcontainer_unlock(devfd, object_offset);
     memset(data, 0, max_size_of_objects_with_buffer);
